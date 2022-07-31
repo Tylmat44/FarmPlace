77c515e7393f31b2e4b16cd20e447e4b2bdf625a
Acc <tylmat44@gmail.com>
2022-07-31 12:49:30 -0400

    asdsd

--- Assets/Scripts/WeeklyManager.cs
+++ Assets/Scripts/WeeklyManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public static class WeeklyManager
 {
     public static int weekNumber;
+    public static List<bool> questsCompleted = new List<bool>();
     public static DateTime STARTDATE = new DateTime(2022, 7, 18, 0, 0, 0);
 
     public static void loadWeeklyInfo()
@@ -21,19 +22,74 @@ public static class WeeklyManager
         command.CommandText = "INSERT OR IGNORE INTO WEEKLY_INFO(info) VALUES(@weeklyString)";
         var parameter = command.CreateParameter();
         parameter.ParameterName = "@weeklytString";
-        parameter.Value = EncryptedXmlSerializer.EncryptData(DateTime.UtcNow.Ticks.ToString() + ",0,0");
+        parameter.Value = EncryptedXmlSerializer.EncryptData(((int)((DateTime.UtcNow - STARTDATE).TotalDays / 7)).ToString() + ",0,0,0,0,0");
         command.Parameters.Add(parameter);
 
         command.ExecuteNonQuery();
         command = DataManager.DatabaseConnection.CreateCommand();
-        command.CommandText = "SELECT * FROM ACCOUNT_INFO;";
+        command.CommandText = "SELECT * FROM WEEKLY_INFO;";
         IDataReader rdr = command.ExecuteReader();
         rdr.Read();
         string[] results = EncryptedXmlSerializer.DecryptData(rdr.GetString(1)).Split(',');
-        LastLogin = DateTime.FromBinary(long.Parse(results[0]));
-        Xp = int.Parse(results[1]);
-        Coins = int.Parse(results[2]);
-        Debug.Log(LastLogin);
+
+        if (int.Parse(results[0]) < (int)((DateTime.UtcNow - STARTDATE).TotalDays / 7))
+        {
+            weekNumber = (int)((DateTime.UtcNow - STARTDATE).TotalDays / 7);
+            for(int i = 0; i < 5; i++)
+            {
+                questsCompleted.Add(false);
+            }
+        } else
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                if (int.Parse(results[i]) == 0)
+                {
+                    questsCompleted.Add(false);
+                }
+                else
+                {
+                    questsCompleted.Add(true);
+                }
+            }
+        }
+
+        DataManager.DatabaseConnection.Close();
+    }
+
+    public static void saveAccountInfo()
+    {
+        DataManager.DatabaseConnection.Open();
+        IDbCommand command = DataManager.DatabaseConnection.CreateCommand();
+        command.CommandText = "DROP TABLE WEEKLY_INFO";
+        command.ExecuteNonQuery();
+        command = DataManager.DatabaseConnection.CreateCommand();
+        command.CommandText = "CREATE TABLE IF NOT EXISTS WEEKLY_INFO (" +
+                    "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                    "info TEXT)";
+        command.ExecuteNonQuery();
+        command.CommandText = "INSERT OR IGNORE INTO WEEKLY_INFO(info) VALUES(@weeklyString)";
+        var parameter = command.CreateParameter();
+        List<int> completed = new List<int>();
+
+        foreach(bool value in questsCompleted)
+        {
+            if(value)
+            {
+                completed.Add(1);
+            } else
+            {
+                completed.Add(0);
+            }
+        }
+
+        string encrypt = DateTime.UtcNow.Ticks.ToString() + "," + completed[0] + "," + completed[1] + "," + completed[2] + "," + completed[3] + "," + completed[4] + "," + completed[5];
+        parameter.ParameterName = "@weeklyString";
+        parameter.Value = EncryptedXmlSerializer.EncryptData(encrypt);
+        command.Parameters.Add(parameter);
+
+        command.ExecuteNonQuery();
         DataManager.DatabaseConnection.Close();
     }
 }
+     
---
+++ Assets/Scripts/WeeklyRequest.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeeklyRequest
+{
+    private Resource resource;
+    private int quantity;
+    private int tier;
+    private int coinReward;
+    private int xpAward;
+    private bool specialRequest;
+    private Dictionary<Resource, int> specialRequests;
+
+    public Resource Resource { get => resource; set => resource = value; }
+    public int Quantity { get => quantity; set => quantity = value; }
+    public int Tier { get => tier; set => tier = value; }
+    public int CoinReward { get => coinReward; set => coinReward = value; }
+    public int XpAward { get => xpAward; set => xpAward = value; }
+    public bool SpecialRequest { get => specialRequest; set => specialRequest = value; }
+    public Dictionary<Resource, int> SpecialRequests { get => specialRequests; set => specialRequests = value; }
+
+    public WeeklyRequest(Resource resource, int quantity, int tier, int coinReward, int xpAward, bool specialRequest)
+    {
+        this.Resource = resource;
+        this.Quantity = quantity;
+        this.Tier = tier;
+        this.CoinReward = coinReward;
+        this.XpAward = xpAward;
+        this.SpecialRequest = specialRequest;
+    }
+}
